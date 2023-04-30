6c3c44358b0d30c65ad3e2f93ca11e2957cb6518
Camilo López <camiloj.lopez79@gmail.com>
2023-04-29 21:39:31 -0400

    REST - Aplicar filtros a la consulta

--- src/pages/api/products/controllers/getProductsController.js
+++ src/pages/api/products/controllers/getProductsController.js
@@ -4,17 +4,29 @@ import { db } from '@/database';
 import { getProductsService } from '../services';
 
 
+/**
+ * Handle a GET request to get all products with any query.
+ *
+ * PATH: /api/products
+ * AUTH-REQUIRED: false
+ * ADMIN-REQUIRED: false
+ *
+ * @param { Object } req - The HTTP request object.
+ * @param { Object } res - The HTTP response object.
+ * @returns { void }
+ */
 const getProductsController = async ( req, res ) => {
   try {
     await db.connect();
 
-    const { products } = await getProductsService();
+    const { totalCountProducts, totalResponseProucts } = await getProductsService( req );
 
     await db.disconnect();
 
     res.status( 200 ).json({
       ok: true,
-      products
+      totalCountProducts,
+      totalResponseProucts
     });
   
   } catch ( error ) {
--- src/pages/api/products/services/getProductsService.js
+++ src/pages/api/products/services/getProductsService.js
@@ -1,15 +1,34 @@
+// Database
+import { SHOP_CONSTANTS } from '@/database';
 // Models
 import { Product } from '@/models';
 
 
-const getProductsService = async () => {
+/**
+ * Service for retrieving products based on provided query parameters
+ *
+ * @param {Object} req - Express request object containing query parameters
+ * @returns {Object} - An object containing the total count of products and an array of products
+ */
+const getProductsService = async ( req ) => {
+  const { gender = 'all' } = req.query;
+  let condition = {};
+
+  if ( gender !== 'all' && SHOP_CONSTANTS.validGenders.includes( gender ) ) {
+    condition = { gender }
+  }
+
   try {
-    const products = await Product.find()
-      .select( 'title images price inStock slug -_id' )
-      .lean()
+    const [ totalCountProducts, totalResponseProucts ] = await Promise.all([
+      Product.countDocuments( condition ),
+      Product.find( condition )
+        .select( 'title images price inStock slug -_id' )
+        .lean()
+    ]);
 
     return {
-      products
+      totalCountProducts,
+      totalResponseProucts
     }
   } catch ( error ) {
     console.log( `${ '[SERVICE.GET-PRODUCTS]'.bgRed }: ${ error }` );
