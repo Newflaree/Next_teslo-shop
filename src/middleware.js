0d5f6fe1f580983801b387e3edf18d18254dd1e9
Camilo López <camiloj.lopez79@gmail.com>
2023-05-26 01:12:30 -0400

    Next 13 - Middleware con Next Auth

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,9 @@
+import {getToken} from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 
 export async function middleware( req ) {
+  /*
   if ( req.nextUrl.pathname.startsWith( '/checkout' ) ) {
     const token = req.cookies.get( 'token' )?.value || '';
     const requestedPage = req.nextUrl.pathname;
@@ -17,4 +19,25 @@ export async function middleware( req ) {
         return NextResponse.rewrite( new URL( `/auth/login?page=${ requestedPage }`, req.url ) )
     }
   }
+    * */
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET || '' })
+  //  Información útil sobre el usuario
+  //console.log({ session });
+  if ( !session ) {
+    const requestedPage = req.nextUrl.pathname;
+    const url = req.nextUrl.clone();
+    url.pathname = '/auth/login';
+    url.search = `page=${ requestedPage }`
+
+    return NextResponse.redirect( url );
+  }
+ 
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/checkout/address',
+    '/checkout/summary'
+  ]
 }
